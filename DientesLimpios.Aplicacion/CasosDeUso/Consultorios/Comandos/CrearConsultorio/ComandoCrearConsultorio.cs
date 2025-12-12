48e2cefb3b4a6a9c2928eab39c8507588bc49b33
PacoChinchay <pacochinchay350@gmail.com>
2025-12-12 15:08:58 -0500

    crear cqrs

---
+++ DientesLimpios.Aplicacion/CasosDeUso/Consultorios/Comandos/CrearConsultorio/ComandoCrearConsultorio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Comandos.CrearConsultorio
+{
+    public class ComandoCrearConsultorio
+    {
+        public required string Nombre { get; set; }
+    }
+}
