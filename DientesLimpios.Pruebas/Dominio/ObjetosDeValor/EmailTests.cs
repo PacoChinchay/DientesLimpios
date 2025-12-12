d882e90b33eb03d725822121c92e253fb634ce82
PacoChinchay <pacochinchay350@gmail.com>
2025-12-11 19:12:11 -0500

    creando test y probando capa de dominio

---
+++ DientesLimpios.Pruebas/Dominio/ObjetosDeValor/EmailTests.cs
@@ -0,0 +1,26 @@
+﻿using DientesLimpios.Dominio.Excepciones;
+using DientesLimpios.Dominio.ObjetosDeValor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Pruebas.Dominio.ObjetosDeValor
+{
+    [TestClass]
+    public class EmailTests
+    {
+        [TestMethod]
+        public void Constructor_EmailNulo_LanzaExcepcion()
+        {
+            Assert.ThrowsExactly<ExcepcionDeReglaDeNegocio>(() => new Email(null!));
+        }
+
+        [TestMethod]
+        public void Constructor_EmailSinArroba_LanzaExcepcion()
+        {
+            Assert.ThrowsExactly<ExcepcionDeReglaDeNegocio>(() => new Email("paco.com"));
+        }
+    }
+}
---
+++ DientesLimpios.Pruebas/Dominio/ObjetosDeValor/IntervaloDeTiempoTest.cs
@@ -0,0 +1,27 @@
+﻿using DientesLimpios.Dominio.Excepciones;
+using DientesLimpios.Dominio.ObjetosDeValor;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Pruebas.Dominio.ObjetosDeValor
+{
+    [TestClass]
+    public class IntervaloDeTiempoTest
+    {
+        [TestMethod]
+        public void Constructor_FechaInicioPosteriorFechaFin_LanzaExcepcion()
+        {
+            Assert.ThrowsExactly<ExcepcionDeReglaDeNegocio>(() => new IntervaloDeTiempo(DateTime.UtcNow, DateTime.UtcNow.AddDays(-1)));
+        }
+
+        [TestMethod]
+        public void Contructor_ParametrosValidos_NoLanzaExcepcion()
+        {
+            new IntervaloDeTiempo(DateTime.UtcNow, DateTime.UtcNow.AddMinutes(30));
+        }
+    }
+}
