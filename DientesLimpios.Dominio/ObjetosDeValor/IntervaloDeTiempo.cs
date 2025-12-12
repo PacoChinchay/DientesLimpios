ca70498bca03bf8ed5a0861931fae53bb8785bb1
PacoChinchay <pacochinchay350@gmail.com>
2025-12-11 19:11:54 -0500

    Separando los obejetos de valor

---
+++ DientesLimpios.Dominio/ObjetosDeValor/IntervaloDeTiempo.cs
@@ -0,0 +1,27 @@
+﻿using DientesLimpios.Dominio.Excepciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Dominio.ObjetosDeValor
+{
+    public record IntervaloDeTiempo
+    {
+        public DateTime Inicio { get; set; }
+        public DateTime Fin { get; set; }
+
+        public IntervaloDeTiempo(DateTime inicio, DateTime fin)
+        {
+
+            if (inicio >= fin)
+            {
+                throw new ExcepcionDeReglaDeNegocio("La fecha de inicio no puede ser posterior a la fecha fin");
+            }
+
+            Inicio = inicio;
+            Fin = fin;
+        }
+    }
+}
