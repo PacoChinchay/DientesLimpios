b8463d90c8df8ed044ca730914f21ccf07636f15
PacoChinchay <pacochinchay350@gmail.com>
2025-12-12 22:31:05 -0500

    creación de tests para el mediador

---
+++ DientesLimpios.Pruebas/Aplicacion/Utilidades/Mediador/MediadorSimpleTests.cs
@@ -0,0 +1,57 @@
+﻿using DientesLimpios.Aplicacion.Excepciones;
+using DientesLimpios.Aplicacion.Utilidades.Mediador;
+using Microsoft.Testing.Platform.Requests;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Pruebas.Aplicacion.Utilidades.Mediador
+{
+    [TestClass]
+    public class MediadorSimpleTests
+    {
+        public class RequestFalso: IRequest<string> { }
+        public class HandlerFalso: IRequestHandler<RequestFalso, string>
+        {
+            public Task<string> Handle(RequestFalso request)
+            {
+                return Task.FromResult("Respuesta correcta");
+            }
+        }
+
+        [TestMethod]
+        public async Task Send_LlamaMetodoHandler()
+        {
+            var request = new RequestFalso();
+            var casoDeUsoMock = Substitute.For<IRequestHandler<RequestFalso, string>>();
+            var serviceProvider = Substitute.For<IServiceProvider>();
+
+            serviceProvider
+                .GetService(typeof(IRequestHandler<RequestFalso, string>))
+                .Returns(casoDeUsoMock);
+
+            var mediador =  new MediadorSimple(serviceProvider);
+            var resultado = await mediador.Send(request);
+
+            await casoDeUsoMock.Received(1).Handle(request);
+        }
+
+        [TestMethod]
+        public async Task Send_SinHandlerRegistrado_LanzaExcepcion()
+        {
+            var request = new RequestFalso();
+            var casoDeUsoMock = Substitute.For<IRequestHandler<RequestFalso, string>>();
+            var serviceProvider = Substitute.For<IServiceProvider>();
+
+            var mediador = new MediadorSimple(serviceProvider);
+
+            await Assert.ThrowsExactlyAsync<ExcepcionDeMediador>(async () =>
+            {
+                var resultado = await mediador.Send(request);
+            });
+        }
+    }
+}
