9fc00e8290a08bee703583d33184ce99bcd57de9
PacoChinchay <pacochinchay350@gmail.com>
2025-12-12 21:35:11 -0500

    crear mediator pattern

---
+++ DientesLimpios.Aplicacion/Utilidades/Mediador/MediadorSimple.cs
@@ -0,0 +1,31 @@
+﻿using DientesLimpios.Aplicacion.Excepciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Aplicacion.Utilidades.Mediador
+{
+    public class MediadorSimple: IMediator
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public MediadorSimple(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+        public async Task<TResponse> Send<TResponse>(IRequest<TResponse> request)
+        {
+            var tipoCasoDeUso = typeof(IRequestHandler<,>).MakeGenericType(request.GetType(), typeof(TResponse));
+            var casoDeUSo = _serviceProvider.GetService(tipoCasoDeUso);
+
+            if (casoDeUSo is null)
+            {
+                throw new ExcepcionDeMediador($"No se encontró un hanlder para {request.GetType().Name}");
+            }
+
+            var metodo = tipoCasoDeUso.GetMethod("Handle")!;
+            return await (Task<TResponse>)metodo.Invoke(casoDeUSo, new object[] { request })!;
+        }
+    }
+}
