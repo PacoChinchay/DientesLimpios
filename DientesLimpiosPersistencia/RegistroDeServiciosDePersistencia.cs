f73370ab2df41278cfa62f6c0a427b7d0854057a
PacoChinchay <pacochinchay350@gmail.com>
2025-12-14 15:52:39 -0500

    configuracion e implementado de los repositorios en proyecto infraestructura

---
+++ DientesLimpiosPersistencia/RegistroDeServiciosDePersistencia.cs
@@ -0,0 +1,25 @@
+﻿using DientesLimpios.Aplicacion.Contratos.Repositorios;
+using DientesLimpios.Persistencia.Repositorios;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Persistencia
+{
+    public static class RegistroDeServiciosDePersistencia
+    {
+        public static IServiceCollection AgregarSerciciosDePersistencia(this IServiceCollection services)
+        {
+            services.AddDbContext<DientesLimpiosDbContext>(options =>
+            {
+                options.UseSqlServer("name=DientesLimpiosConnectionString");
+            });
+            services.AddScoped<IRepositorioConsultorios, RepositorioConsultorios>();
+            return services;
+        }
+    }
+}
