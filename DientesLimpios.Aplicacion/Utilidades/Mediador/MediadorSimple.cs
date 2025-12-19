767dde5bf8617c1de7fafbde087ec0b87f15223a
PacoChinchay <pacochinchay350@gmail.com>
2025-12-19 14:55:36 -0500

    refactorizando mediador

--- DientesLimpios.Aplicacion/Utilidades/Mediador/MediadorSimple.cs
+++ DientesLimpios.Aplicacion/Utilidades/Mediador/MediadorSimple.cs
@@ -18,24 +18,7 @@ namespace DientesLimpios.Aplicacion.Utilidades.Mediador
         }
         public async Task<TResponse> Send<TResponse>(IRequest<TResponse> request)
         {
-            var tipoValidador = typeof(IValidator<>).MakeGenericType(request.GetType());
-            var validador = _serviceProvider.GetService(tipoValidador);
-
-            if (validador is not null)
-            {
-                var metodoValidar = tipoValidador.GetMethod("ValidateAsync");
-                var tareaValidar = (Task)metodoValidar!.Invoke(validador, new object[] {request, CancellationToken.None})!;
-
-                await tareaValidar.ConfigureAwait(false);
-
-                var resultado = tareaValidar.GetType().GetProperty("Result");
-                var validationResult = (ValidationResult)resultado!.GetValue(tareaValidar)!;
-
-                if (!validationResult.IsValid)
-                {
-                    throw new ExcepcionDeValidacion(validationResult);
-                }
-            }
+            await RealizarValidaciones(request);
 
             var tipoCasoDeUso = typeof(IRequestHandler<,>).MakeGenericType(request.GetType(), typeof(TResponse));
             var casoDeUSo = _serviceProvider.GetService(tipoCasoDeUso);
@@ -47,6 +30,44 @@ namespace DientesLimpios.Aplicacion.Utilidades.Mediador
 
             var metodo = tipoCasoDeUso.GetMethod("Handle")!;
             return await (Task<TResponse>)metodo.Invoke(casoDeUSo, new object[] { request })!;
+        }
+
+        public async Task Send(IRequest request)
+        {
+            await RealizarValidaciones(request);
+            
+            var tipoCasoDeUso = typeof(IRequestHandler<>).MakeGenericType(request.GetType());
+            var casoDeUso = _serviceProvider.GetService(tipoCasoDeUso);
+
+            if(casoDeUso is null)
+            {
+                throw new ExcepcionDeMediador($"Nose ecnotró una handler para {request.GetType().Name}");
+            }
+
+            var metodo = tipoCasoDeUso.GetMethod("Handle")!;
+            await (Task)metodo.Invoke(casoDeUso, new object[] { request })!;
+        }
+
+        private async Task RealizarValidaciones(Object request)
+        {
+            var tipoValidador = typeof(IValidator<>).MakeGenericType(request.GetType());
+            var validador = _serviceProvider.GetService(tipoValidador);
+
+            if (validador is not null)
+            {
+                var metodoValidar = tipoValidador.GetMethod("ValidateAsync");
+                var tareaValidar = (Task)metodoValidar!.Invoke(validador, new object[] { request, CancellationToken.None })!;
+
+                await tareaValidar.ConfigureAwait(false);
+
+                var resultado = tareaValidar.GetType().GetProperty("Result");
+                var validationResult = (ValidationResult)resultado!.GetValue(tareaValidar)!;
+
+                if (!validationResult.IsValid)
+                {
+                    throw new ExcepcionDeValidacion(validationResult);
+                }
+            }
         }
     }
 }
---
+++ DientesLimpios.Pruebas/Aplicacion/CasosDeUso/Consultorios/CasoDeUsoObtenerListadoConsultoriosTests.cs
@@ -0,0 +1,59 @@
+﻿using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Consultas.ObtenerListadoConsultorios;
+using DientesLimpios.Aplicacion.Contratos.Repositorios;
+using DientesLimpios.Dominio.Entidades;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Pruebas.Aplicacion.CasosDeUso.Consultorios
+{
+    [TestClass]
+    public class CasoDeUsoObtenerListadoConsultoriosTests
+    {
+        private IRepositorioConsultorios _repositorio;
+        private CasoDeUsoObtenerListadoConsultorios _casoDeUso;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _repositorio = Substitute.For<IRepositorioConsultorios>();
+            _casoDeUso = new CasoDeUsoObtenerListadoConsultorios(_repositorio);
+        }
+
+        [TestMethod]
+        public async Task Handle_CuandoHayConsultorios_RetornaListaDeConsultoriosListadoDTO()
+        {
+            var consultorios = new List<Consultorio>
+                {
+                    new Consultorio("Consultorio A"),
+                };
+
+            _repositorio.ObtenerTodos().Returns(consultorios);
+
+            var esperado = consultorios.Select(c => new ConsultorioListadoDTO { Id = c.Id, Nombre = c.Nombre }).ToList();
+            var resultado = await _casoDeUso.Handle(new ConsultaObtenerListadoConsultorios());
+
+            Assert.AreEqual(esperado.Count, resultado.Count);
+
+            for (int i = 0; i < esperado.Count; i++)
+            {
+                Assert.AreEqual(esperado[i].Id, resultado[i].Id);
+                Assert.AreEqual(esperado[i].Nombre, resultado[i].Nombre);
+            }
+        }
+
+        [TestMethod]
+        public async Task Handle_CuandoNoHayConsultorios_RetornaListaVacia()
+        {
+            _repositorio.ObtenerTodos().Returns(new List<Consultorio>());
+
+            var resultado = await _casoDeUso.Handle(new ConsultaObtenerListadoConsultorios());
+
+            Assert.IsNotNull(resultado);
+            Assert.AreEqual(0, resultado.Count);
+        }
+    }
+}
