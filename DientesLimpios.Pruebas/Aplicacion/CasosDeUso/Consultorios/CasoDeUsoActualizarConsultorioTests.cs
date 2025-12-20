2e3e050383336059ba6bfa8b3563cdf806c0e1b3
PacoChinchay <pacochinchay350@gmail.com>
2025-12-19 22:17:00 -0500

    pruebas de actualizar consultorio

---
+++ DientesLimpios.Pruebas/Aplicacion/CasosDeUso/Consultorios/CasoDeUsoActualizarConsultorioTests.cs
@@ -0,0 +1,72 @@
+﻿using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Comandos.ActualizarConsultorio;
+using DientesLimpios.Aplicacion.Contratos.Persistencia;
+using DientesLimpios.Aplicacion.Contratos.Repositorios;
+using DientesLimpios.Aplicacion.Excepciones;
+using DientesLimpios.Dominio.Entidades;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using NSubstitute.ReturnsExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DientesLimpios.Pruebas.Aplicacion.CasosDeUso.Consultorios
+{
+    [TestClass]
+    public class CasoDeUsoActualizarConsultorioTests
+    {
+        private IRepositorioConsultorios _repositorio;
+        private IUnidadDeTrabajo _unidadDeTrabajo;
+        private CasoDeUsoActualizarConsutorio _casoDeUso;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _repositorio = Substitute.For<IRepositorioConsultorios>();
+            _unidadDeTrabajo = Substitute.For<IUnidadDeTrabajo>();
+            _casoDeUso = new CasoDeUsoActualizarConsutorio(_repositorio, _unidadDeTrabajo);
+        }
+
+        [TestMethod]
+        public async Task Handle_CuandoConsultorioExiste_ActualizaNombreYPersiste()
+        {
+            var consultorio = new Consultorio("Consultorio A");
+            var id = consultorio.Id;
+            var comando = new ComandoActualizarConsultorio { Id = id , Nombre = "Nuevo nombre"};
+
+            _repositorio.ObtenerPorId(id).Returns(consultorio);
+
+            await _casoDeUso.Handle(comando);
+            await _repositorio.Received(1).Actualizar(consultorio);
+            await _unidadDeTrabajo.Received(1).Persistir();
+        }
+
+        [TestMethod]
+        public async Task Handle_CuandoConsultorioNoExiste_LanzaExcepcionNoEncontrado()
+        {
+            var comando = new ComandoActualizarConsultorio { Id = Guid.NewGuid(), Nombre = "Nombre" };
+            _repositorio.ObtenerPorId(comando.Id).ReturnsNull();
+
+            await Assert.ThrowsExactlyAsync<ExcepcionNoEncontrado>(async () =>
+            {
+                await _casoDeUso.Handle(comando);
+            });
+        }
+
+        [TestMethod]
+        public async Task Handle_CuandoOcurreExcepcionalActualizar_LlamaAReversarYLanzaExcepcion()
+        {
+            var consultorio = new Consultorio("Consultorio A");
+            var id = consultorio.Id;
+            var comando = new ComandoActualizarConsultorio { Id = id, Nombre = "Consultorio B" };
+
+            _repositorio.ObtenerPorId(id).Returns(consultorio);
+            _repositorio.Actualizar(consultorio).Throws(new InvalidOperationException("Error al actualizar"));
+
+            await Assert.ThrowsExactlyAsync<InvalidOperationException>(() => _casoDeUso.Handle(comando));
+            await _unidadDeTrabajo.Received(1).Reversar();
+        }
+    }
+}
