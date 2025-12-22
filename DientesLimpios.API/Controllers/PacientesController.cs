57a1a0ee0aee6db133d1f5c195c135b26d12441a
PacoChinchay <pacochinchay350@gmail.com>
2025-12-21 23:09:33 -0500

    crear controller paciente

---
+++ DientesLimpios.API/Controllers/PacientesController.cs
@@ -0,0 +1,27 @@
+﻿using DientesLimpios.API.DTOs.Pacientes;
+using DientesLimpios.Aplicacion.CasosDeUso.Pacientes.Comandos.CrearPaciente;
+using DientesLimpios.Aplicacion.Utilidades.Mediador;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DientesLimpios.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PacientesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public PacientesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CrearPacienteDTO crearPacienteDTO)
+        {
+            var comando = new ComandoCrearPaciente { Nombre = crearPacienteDTO.Nombre, Email = crearPacienteDTO.Email };
+            await _mediator.Send(comando);
+            return Ok();
+        }
+    }
+}
